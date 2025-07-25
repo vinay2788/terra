207dd11bdbd0dd28866cf691ed2d00e0c8b0a4f9
Vinayak <vinaybali121@gmail.com>
2025-07-25 17:42:10 +0530

    state update

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,10 @@
 resource "aws_instance" "terrakube" {
-    ami                                  = "ami-0f918f7e67a3323f0"
-    associate_public_ip_address          = true
-    instance_type                        = "t2.large"
-    key_name                             = "vinay-legitbytes"
-  
+  ami                         = "ami-0f918f7e67a3323f0"
+  associate_public_ip_address = true
+  instance_type               = "t2.large"
+  key_name                    = "vinay-legitbytes"
+
+  tags = {
+    Name = "terrakube"
+  }
 }
