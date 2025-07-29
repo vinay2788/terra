b24e0dbf78b131f3bd6d2a6367e459960818c135
Vinayak <vinaybali121@gmail.com>
2025-07-30 01:31:34 +0530

    terra

--- sg.tf
+++ sg.tf
@@ -30,19 +30,23 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.test.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "tcp" # semantically equivalent to all ports
+  from_port = 3000
+  to_port = 3000
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   security_group_id = aws_security_group.test.id
   cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "tcp" # semantically equivalent to all ports
+  from_port = 4000
+  to_port = 4000
 }
 resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
     security_group_id = aws_security_group.test.id
-    cidr_ipv4         = "0.0.0.0/0"
-    from_port         = 0
-    to_port           = 0
-    ip_protocol       = "-1"
+    cidr_ipv4         = "10.0.0.0/16"
+    from_port         = 5000
+    to_port           = 5000
+    ip_protocol       = "tcp"
   
 }
